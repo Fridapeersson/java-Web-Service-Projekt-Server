f440855c9062099c94bf785b0cf66b6aa9bc7d10
Frida <fridapeersson@gmail.com>
2023-03-25 20:16:18 +0100

    UserController done

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,42 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController
 public class UserController {
 	
+	private UserService userService;
+	
 	@Autowired
-	public UserController(userService userService) {
-		
+	public void userController(UserService userService) {
+		this.userService = userService;
 	}
-
+	
+	
+	@GetMapping("/user")
+	public User getUser(@RequestParam Integer id) {
+		Optional<User> user = userService.getUser(id);
+		if(user.isPresent()) {
+			return (User) user.get();
+		}
+		return null;
+	}
+	
+	@GetMapping("/user/{id}")
+	public User getUserByPathVariable(@PathVariable Integer id) {
+		Optional<User> user = userService.getUser(id);
+		if(user.isPresent()) {
+			return user.get();
+		}
+		return null;
+	}
 }
